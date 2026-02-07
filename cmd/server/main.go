e7a4d7f354b5308fb2407910738e8da7f0462757
Maxim <kudriashoffmaksim@yandex.ru>
2026-02-07 19:46:19 +0300

    add chi router and health endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
-	http.ListenAndServe(":8080", nil)
+	router := chi.NewRouter()
+
+	router.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+
+	http.ListenAndServe(":8080", router)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module bookshelf-api
 
 go 1.25.6
+
+require (
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/go-chi/chi/v5 v5.2.5 // indirect
+)
