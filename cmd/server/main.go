3bf82acc004a1f9c34697a7084c3b0c97575fb87
Maxim <kudriashoffmaksim@yandex.ru>
2026-02-07 23:21:39 +0300

    load env

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"bookshelf-api/internal/config"
 	"bookshelf-api/internal/routes"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("no .env file found")
+	}
+
 	router := chi.NewRouter()
 
 	routes.Register(router)
