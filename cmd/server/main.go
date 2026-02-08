bd5b2d034e6b5c0c36024c7ac7bd6b7c8208c3fb
Maxim <kudriashoffmaksim@yandex.ru>
2026-02-08 18:40:18 +0300

    docker compose app

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"bookshelf-api/internal/config"
 	"bookshelf-api/internal/routes"
+	"context"
+	"database/sql"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
+	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/joho/godotenv"
 )
 
@@ -16,12 +20,25 @@ func main() {
 		log.Println("no .env file found")
 	}
 
-	router := chi.NewRouter()
+	cfg := config.Load()
 
+	db, err := sql.Open("pgx", cfg.DBDSN)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := db.PingContext(ctx); err != nil {
+		log.Fatal("failed to connect to db:", err)
+	}
+
+	router := chi.NewRouter()
 	routes.Register(router)
 
-	cfg := config.Load()
 	addr := ":" + cfg.HTTPPort
+	log.Println("starting server on", addr)
 
 	http.ListenAndServe(addr, router)
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,18 +1,28 @@
 package config
 
-import "os"
+import (
+	"log"
+	"os"
+)
 
 type Config struct {
 	HTTPPort string
+	DBDSN    string
 }
 
 func Load() *Config {
 	port := os.Getenv("HTTP_PORT")
+	dbDsn := os.Getenv("DB_DSN")
 	if port == "" {
 		port = "8080"
+	}
+
+	if dbDsn == "" {
+		log.Fatal("DB_DSN is required")
 	}
 
 	return &Config{
 		HTTPPort: port,
 	}
+
 }
